e3e3d504fbca224b8983cb283a527369213405c1
roqu1 <snipklas@gmail.com>
2023-02-14 12:30:45 +0100

    canvas

--- src/main/java/com/example/canvas/HelloApplication.java
+++ src/main/java/com/example/canvas/HelloApplication.java
@@ -5,21 +5,28 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.*;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import java.io.IOException;
 
+
 public class HelloApplication extends Application {
+    private Canvas canvas = new Canvas(300, 250);
+    private GraphicsContext gc = canvas.getGraphicsContext2D();
+    private Group root = new Group();
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Drawing Operations Test");
-        Group root = new Group();
-        Canvas canvas = new Canvas(300,250);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
         drawShapes(gc);
         root.getChildren().add(canvas);
-        stage.setScene(new Scene(root));
+        stage.setScene(new Scene(root,400,400));
+        moveCanvas(60,50);
+        drawDShape();
+        drawRadialGradient(Color.RED, Color.YELLOW);
+        drawLinearGradient(Color.BLUE, Color.GREEN);
+        drawDropShadow(Color.GRAY, Color.BLUE, Color.GREEN, Color.RED);
         stage.show();
     }
 
@@ -30,6 +37,39 @@ public class HelloApplication extends Application {
     private void moveCanvas(int x, int y) {
         canvas.setTranslateX(x);
         canvas.setTranslateY(y);
+    }
+
+    private void drawDShape() {
+        gc.beginPath();
+        gc.moveTo(50, 50);
+        gc.bezierCurveTo(150, 20, 150, 150, 75, 150);
+        gc.closePath();
+    }
+
+    private void drawRadialGradient(Color firstColor, Color lastColor) {
+        gc.setFill(new RadialGradient(0, 0, 0.5, 0.5, 0.1, true,
+                CycleMethod.REFLECT,
+                new Stop(0.0, firstColor),
+                new Stop(1.0, lastColor)) );
+        gc.fill();
+    }
+
+    private void drawDropShadow(Color firstColor, Color secondColor,
+                                Color thirdColor, Color fourthColor) {
+        gc.applyEffect(new DropShadow(20, 20, 0, firstColor));
+        gc.applyEffect(new DropShadow(20, 0, 20, secondColor));
+        gc.applyEffect(new DropShadow(20, -20, 0, thirdColor));
+        gc.applyEffect(new DropShadow(20, 0, -20, fourthColor));
+    }
+
+    private void drawLinearGradient(Color firstColor, Color secondColor) {
+        LinearGradient lg = new LinearGradient(0, 0, 1, 1, true,
+                CycleMethod.REFLECT,
+                new Stop(0.0, firstColor),
+                new Stop(1.0, secondColor));
+        gc.setStroke(lg);
+        gc.setLineWidth(20);
+        gc.stroke();
     }
 
     private void drawShapes(GraphicsContext gc) {
